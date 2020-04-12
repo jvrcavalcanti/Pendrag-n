8b8c7b386132b28e42aa4b3b8ffc9ace0003f728
jvrcavalcanti <jonnyvictor01@gmail.com>
2020-04-12 03:29:41 -0300

    Route test

--- routes.php
+++ routes.php
@@ -4,6 +4,4 @@ $app = new \Accolon\Route\Route;
 
 $app->get("/", fn() => view("welcome"));
 
-$app->get("/user/{id}", fn($request) => $request->get("id"));
-
 $app->get("/auth/login", "AuthController.login", \App\Middleware\JsonResponse::class);
