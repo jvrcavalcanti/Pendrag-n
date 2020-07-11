9c0ff97e1057edb372f535a82daa0da555b1197d
Jvrcavalcanti <jonnyvictor01@gmail.com>
2020-07-11 20:35:30 -0300

    Cli

--- app/Model/User.php
+++ app/Model/User.php
@@ -3,8 +3,11 @@
 namespace App\Model;
 
 use Accolon\DataLayer\Model;
+use Pendragon\Framework\Auth\HasApiToken;
 
 class User extends Model
 {
+    use HasApiToken;
+
     protected string $table = "users";
 }
